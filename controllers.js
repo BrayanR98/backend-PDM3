b26e800a4a32ee4015e68949cd8eef5b4399585e
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-11-12 09:10:44 -0500

    se corrige require

--- controllers.js
+++ controllers.js
@@ -1,88 +1,78 @@
-const { User, Venta } = require("./models.js");
+import { User, Venta } from "./models.js";
 
 
 // ------- User
 
-exports.readUsers = (req, res) =>
-    User.find({}, (err, data) => {
+export function readUsers(req, res)    {     return User.find({}, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
-exports.readUser = (req, res) =>
-    User.findOne({ idvend: req.params.id }, (err, data) => {
+export function readUser(req, res)    {     return User.findOne({ idvend: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
-exports.deleteUser = (req, res) =>
-    User.findOneAndRemove({ idvend: req.params.id }, (err, data) => {
+export function deleteUser(req, res)    {     return User.findOneAndRemove({ idvend: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
-exports.updateUser = (req, res) =>
-    User.findOneAndUpdate(
+export function updateUser(req, res)    {     return User.findOneAndUpdate(
         { _id: req.params.id },
         { $set: { nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision } }, 
         (err, data) => {
             if (err) res.json({ error: err });
             else     res.json(data);
         }
-    );
+    );     }
 
 
-exports.createUser = (req, res) =>
-    new User({ idvend: req.body.idvend, nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision })
+export function createUser(req, res)    {     return new User({ idvend: req.body.idvend, nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision })
     .save((err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
 
 // ------ ARTÍCULOS
 
-exports.readVentas = (req, res) =>
-    Venta.find({}, (err, data) => {
+export function readVentas(req, res)    {     return Venta.find({}, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
-exports.readVenta = (req, res) =>
-    Venta.findOne({ _id: req.params.id }, (err, data) => {
+export function readVenta(req, res)    {     return Venta.findOne({ _id: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
-exports.deleteVenta = (req, res) =>
-    Venta.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+export function deleteVenta(req, res)    {     return Venta.findOneAndRemove({ _id: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
 
 
-exports.updateVenta = (req, res) =>
-    Venta.findOneAndUpdate(
+export function updateVenta(req, res)    {     return Venta.findOneAndUpdate(
         { _id: req.params.id },
         { $set: { idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa } }, 
         (err, data) => {
             if (err) res.json({ error: err });
             else     res.json(data);
         }
-    );
+    );     }
 
 
-exports.createVenta = (req, res) =>
-    new Venta({ idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa })
+export function createVenta(req, res)    {     return new Venta({ idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa })
     .save((err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });
+    });     }
 
