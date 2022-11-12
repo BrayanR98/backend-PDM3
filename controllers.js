21e2cfa41f7534168907ad070ce14fffeec9afde
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-11-12 12:17:19 -0500

    dbremota

--- controllers.js
+++ controllers.js
@@ -1,78 +1,88 @@
-import { User, Venta } from "./models.js";
+const { User, Venta } = require("./models.js");
 
 
 // ------- User
 
-export function readUsers(req, res)    {     return User.find({}, (err, data) => {
+exports.readUsers = (req, res) =>
+    User.find({}, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
-export function readUser(req, res)    {     return User.findOne({ idvend: req.params.id }, (err, data) => {
+exports.readUser = (req, res) =>
+    User.findOne({ idvend: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
-export function deleteUser(req, res)    {     return User.findOneAndRemove({ idvend: req.params.id }, (err, data) => {
+exports.deleteUser = (req, res) =>
+    User.findOneAndRemove({ idvend: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
-export function updateUser(req, res)    {     return User.findOneAndUpdate(
+exports.updateUser = (req, res) =>
+    User.findOneAndUpdate(
         { _id: req.params.id },
         { $set: { nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision } }, 
         (err, data) => {
             if (err) res.json({ error: err });
             else     res.json(data);
         }
-    );     }
+    );
 
 
-export function createUser(req, res)    {     return new User({ idvend: req.body.idvend, nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision })
+exports.createUser = (req, res) =>
+    new User({ idvend: req.body.idvend, nombre: req.body.nombre, correo: req.body.correo,rol:req.body.rol , totalcomision: req.body.totalcomision })
     .save((err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
 
 // ------ ARTÍCULOS
 
-export function readVentas(req, res)    {     return Venta.find({}, (err, data) => {
+exports.readVentas = (req, res) =>
+    Venta.find({}, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
-export function readVenta(req, res)    {     return Venta.findOne({ _id: req.params.id }, (err, data) => {
+exports.readVenta = (req, res) =>
+    Venta.findOne({ _id: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
-export function deleteVenta(req, res)    {     return Venta.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+exports.deleteVenta = (req, res) =>
+    Venta.findOneAndRemove({ _id: req.params.id }, (err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
 
 
-export function updateVenta(req, res)    {     return Venta.findOneAndUpdate(
+exports.updateVenta = (req, res) =>
+    Venta.findOneAndUpdate(
         { _id: req.params.id },
         { $set: { idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa } }, 
         (err, data) => {
             if (err) res.json({ error: err });
             else     res.json(data);
         }
-    );     }
+    );
 
 
-export function createVenta(req, res)    {     return new Venta({ idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa })
+exports.createVenta = (req, res) =>
+    new Venta({ idvend: req.body.idvend, zona: req.body.zona , fecha:req.body.fecha,valorventa:req.body.valorventa })
     .save((err, data) => {
         if (err) res.json({ error: err });
         else     res.json(data);
-    });     }
+    });
 
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const apiRoutes  = require('./routes');
 
 const app    = express();
 const PORT   = process.env.PORT || 3000;
-const DB_URI = process.env.DB_URI;
+const DATABASE = process.env.DATABASE;
 
 
 // CONEXIÓN A BASE DE DATOS
-mongoose.connect('mongodb://localhost:27017/dbpdm3')
+mongoose.connect(DATABASE)
     .then(db => console.log("Conexión a BD correcta"))
     .catch(error => console.log("Error al conectarse a la BD" + error));
 
@@ -23,4 +23,4 @@ app.use('/api', apiRoutes);
 app.use(express.static(path.join(__dirname , 'public')));
 
 // SERVIDOR WEB
-app.listen(PORT, () => console.log(`Servidor iniciado en hhtp://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor iniciado en http://localhost:${PORT}`));
